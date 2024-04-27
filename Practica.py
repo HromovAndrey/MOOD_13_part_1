1e3a83645c7c1afcbdf5b273b383c0c425b4490a
user <andrey.gromov.36912@gmail.com>
2024-04-27 19:17:57 +0300

    Лаба

--- Practica.py
+++ Practica.py
@@ -1,6 +1,6 @@
-import pickle
+#import pickle
 
-data = {"login": "world", "passwork" : "123153" }# есть обьек
+#data = {"login": "world", "passwork" : "123153" }# есть обьек
 
 # print(data)
 # serialized_data = pickle.dumps(data)# обьект переводиться в набір байтів
@@ -9,6 +9,18 @@ data = {"login": "world", "passwork" : "123153" }# есть обьек
 #
 # print(type(read_data), read_data)
 
-with open("data.pickle .pk .pk1", "wb") as file:
-    pickle.dump(data, file)
+# with open("data.pickle .pk .pk1") as file:
+#     pickle.dump(data, file)
 # dump значить що не має файла  dumps це з файлами
+# with open("data.pickle", "rb") as file:#читаємо файл
+#     read_data = pickle.load(file)
+# print(type(read_data), read_data)
+
+import pickle
+import  gzip
+
+data = {"login": "world", "password": "12345678"}
+
+with gzip.open("data.gz", "wb") as file:
+    serialized_data = pickle.dumps(data)
+    file.write(serialized_data)
