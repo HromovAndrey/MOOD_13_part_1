693b6ab83f3788c793ddb484c4323ff75b49a1e1
user <andrey.gromov.36912@gmail.com>
2024-05-02 20:17:41 +0300

    Лаба

--- Laba.py
+++ Laba.py
@@ -2,31 +2,16 @@
 # Користувач заповнює з клавіатури список цілих.
 # Стисніть отримані дані та збережіть їх у файл. Після цього
 # завантажте дані з файлу в новий список.
-
 import pickle
-import gzip
+
+number = (int(input("Заповніть з клавіатури список цілих чисел")))
+print(number)
+
+serialized_data = pickle.dumps(number)
+print(f"Serialized data:\n{serialized_data}\n")
+
+deserialized_data = pickle.loads(serialized_data)
+print(f"Deserialized data:\n{deserialized_data}\n")
 
 
-def save_list_to_file(data, filename):
-    with gzip.open(filename, 'wb') as f:
-        pickle.dump(data, f)
 
-def load_list_from_file(filename):
-    with gzip.open(filename, 'rb') as f:
-        loaded_data = pickle.load(f)
-    return loaded_data
-
-input_list = []
-while True:
-    try:
-        number = int(input("Введіть ціле число (або 'q' для завершення): "))
-        input_list.append(number)
-    except ValueError:
-        if input("Ви впевнені, що хочете завершити введення? (y/n): ").lower() == 'y':
-            break
-
-save_list_to_file(input_list, "compressed_list.pklz")
-print("Список успішно збережено у файл.")
-
-loaded_list = load_list_from_file("compressed_list.pklz")
-print("Дані успішно завантажено з файлу:", loaded_list)
